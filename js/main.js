32abf6ece32b32c4f7dc7e8aa71fc8a2b9f001f9
Anders Jensen <43644886+Andnj@users.noreply.github.com>
2019-11-18 17:04:35 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+function handleSelect(elm) {
+    window.location = elm.value + ".html";
+};
+
+
+const pro_left = document.querySelectorAll('.test');
+const pro_right = document.querySelectorAll('.test2');
+
+let options = {
+    root: null,
+    rootMargin: '0px 0px 0px 0px',
+    threshold: 1.0
+};
+
+function callback_1(entries) {
+    entries.forEach(entrie => {
+        console.log(entrie);
+        if (entrie.intersectionRatio > 0.0) {
+            entrie.target.style.animation = 'anim1 0.8s forwards ease-out';
+        } else {
+            entrie.target.style.animation = 'none';
+        }
+    });
+}
+
+function callback_2(entries) {
+    entries.forEach(entrie => {
+        console.log(entrie);
+        if (entrie.intersectionRatio > 0.0) {
+            entrie.target.style.animation = 'anim2 0.8s forwards ease-out';
+        } else {
+            entrie.target.style.animation = 'none';
+        }
+    });
+}
+
+
+observer_1 = new IntersectionObserver(callback_1, options);
+observer_2 = new IntersectionObserver(callback_2, options);
+
+pro_left.forEach(pros => {
+    observer_1.observe(pros);
+});
+
+pro_right.forEach(con => {
+    observer_2.observe(con);
+});
