5417705aac9ea7683b1338b7eaf585a333422bbb
Anders Jensen <43644886+Andnj@users.noreply.github.com>
2019-11-18 23:49:48 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ function handleSelect(elm) {
     window.location = elm.value + ".html";
 };
 
+function menu_mobile() {
+    var x = document.getElementById("links_mobile");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+};
 
+
+
+//-------------IntersectionObserver---------------
 const pro_left = document.querySelectorAll('.anim_left');
 const pro_right = document.querySelectorAll('.anim_right');
 
@@ -14,7 +25,7 @@ let options = {
 
 function callback_1(entries) {
     entries.forEach(entrie => {
-        console.log(entrie);
+        // console.log(entrie);
         if (entrie.intersectionRatio > 0) {
             entrie.target.style.animation = 'anim1 0.8s forwards ease-out';
         } else {
@@ -25,7 +36,7 @@ function callback_1(entries) {
 
 function callback_2(entries) {
     entries.forEach(entrie => {
-        console.log(entrie);
+        // console.log(entrie);
         if (entrie.intersectionRatio > 0.0) {
             entrie.target.style.animation = 'anim2 0.8s forwards ease-out';
         } else {
